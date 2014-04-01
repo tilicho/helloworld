74cfa17ebd16f040a97247778d83d6a9c7013327
tilicho <ser@losthost.local>
2014-04-02 02:57:37 +0400

    added first file

---
+++ helloworld/helloworld/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by ser on 02/04/14.
+//  Copyright (c) 2014 ser. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
