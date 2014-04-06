c08cbff290deec1ebdd1c93176ad73fb7fd917db
tilicho <berserksmail@gmail.com>
2014-04-07 00:25:46 +0400

    no copyright
    first bug was fixed

--- helloworld/helloworld/main.cpp
+++ helloworld/helloworld/main.cpp
@@ -3,15 +3,12 @@
 //  helloworld
 //
 //  Created by ser on 02/04/14.
-//  Copyright (c) 2014 ser. All rights reserved.
 //
 
 #include <iostream>
 
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
     std::cout << "Hello, World!\n";
     return 0;
 }
